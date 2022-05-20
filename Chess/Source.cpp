d322414ea0fbd45fc3a1eef749672ce6e02976db
Владимир Солдатов <cbb110@rambler.ru>
2022-05-20 09:15:52 +0300

    Set_monitor

--- Chess/Source.cpp
+++ Chess/Source.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#define _WIN32_WINNT 0x0500
+
+#include <windows.h>
+
 using namespace std;
 
 enum TypeFigure {
@@ -11,7 +15,9 @@ class Desk
 {
 	static int count;
 	int size;
+	static Desk* p_instance;
 	int DeskTable[8][8];
+	
 	Desk()
 	{
 		size = 8;
@@ -22,29 +28,41 @@ class Desk
 				DeskTable[i][j] = 0;
 			}
 		}
-		SetCount();
+		
 	}
-	static void SetCount() {
-		if (count == NULL)
-			count = 0;
-		count++;
-	}
+	Desk(const Desk&);
+	Desk& operator=(Desk&);
+	
+
+
 public:
-	static Desk GetInstatces() {
-		if (count > 0)
-			return Desk();
-		else
-		{
-			return *this;
-		}
+	static Desk * GetInstatces() {
+		if (!p_instance)
+			p_instance = new Desk();
+		cout << "Create Desk" << endl;
+		return p_instance;
 
 	}
-	
+	void get_values() {
+		cout << endl;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				cout << DeskTable[i][j] <<"\t";
+			}
+			cout << endl<<endl << endl << endl;
+		}
+	}
+	void set_monotor() {
+		HWND hWindowConsole = GetConsoleWindow();
+		RECT r;
+		GetWindowRect(hWindowConsole, &r); 
+		MoveWindow(hWindowConsole, r.left, r.top, 800, 800, TRUE);
+	}
 	~Desk() {
 		cout << "End Game" << endl;
 	}
 };
-
+Desk* Desk::p_instance = NULL;
  class Figure 
  {
 	 char* name = 0;
@@ -64,11 +82,12 @@ public:
  public:
 	 KonClass(const char* your_name, const char* your_color, int yourColor)
 	 {
-		 name = new char[50];
+		 name = new char[100];
 		 color = new char[50];
-		 strcpy_s(name, sizeof(your_name), your_name);
+		 strcpy_s(name, sizeof(your_name)*2, your_name);
 		 strcpy_s(color, sizeof(your_color), your_color);
 		 colorCode = Black;
+		 cout << name << " created" <<endl;
 	 }
 	 ~KonClass()
 	 {
@@ -99,7 +118,7 @@ public:
 	 {
 		 KonClass blackLeftKon("Left Black Kon", "Black", 1);
 		 KonClass blackRightKon("Right Black Kon", "Black", 1);
-		 cout << "Black Kons created";
+	
 		
 
 	}
@@ -117,7 +136,7 @@ public:
 		 strcpy_s(name, sizeof("First"), "First");
 	 }
 	 ~Game() {
-		 cout << "\nYour name " << name;
+		 cout << "Your name " << name << endl;
 		 delete name;
 	 }
 	 void createFabric()
@@ -125,9 +144,17 @@ public:
 		 FabricBlackFigures blackFabric;
 		 blackFabric.createFigures();
 	 }
+	 void create_desk() {
+		 Desk::GetInstatces();
+	 }
  };
 int main() {
 	Game game;
+	game.createFabric();
+	game.create_desk();
+	Desk* my_desk = Desk::GetInstatces();
+	my_desk->get_values();
+	my_desk->set_monotor();
 
 	return 0;
 }
